3d5e0103530879f80254b31e7d8f7aea3e2364f5
ikthnism01 <ikthnism01@gmail.com>
2019-05-27 15:46:28 +0900

    static resource web service. ejs template, i18n. TODO jsp to ejs ...

--- msreserve.web.ui/app.js
+++ msreserve.web.ui/app.js
@@ -2,23 +2,41 @@
 /**
  * Module dependencies.
  */
-
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , cookieParser = require('cookie-parser')
+  , i18n= require('i18n');
+
+/**
+ * Module routes.
+ */
+var main= require('./routes/main');
 
 var app = express();
 
+i18n.configure({
+	locales: ['en', 'ko']
+	, directory: __dirname + '/messages'
+	, defaultLocale: 'ko'
+	, cookie: 'lang'
+	, queryParameter: 'lang'
+});
+
+app.configure(function() {
+	app.use(i18n.init);
+});
+
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8080);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.use(cookieParser());
+app.use(i18n);
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
-app.use(express.methodOverride());
+app.use(express.methodOverride());	// Get, Post 외의 메소드 사용시 설정 필요
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,9 +45,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', main);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Server listening on port ' + app.get('port'));
 });
---
+++ msreserve.web.ui/routes/main.js
@@ -0,0 +1,6 @@
+/**
+ * http://usejsdoc.org/
+ */
+module.exports= function(req, res, next) {
+	res.render('main');
+};
