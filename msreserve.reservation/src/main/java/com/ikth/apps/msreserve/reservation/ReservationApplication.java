39973e29c24a529ccc70e33b6f4799f7cef60558
ikthnism01 <ikthnism01@gmail.com>
2019-05-30 22:55:23 +0900

    add config server and config client test

--- msreserve.reservation/src/main/java/com/ikth/apps/msreserve/reservation/ReservationApplication.java
+++ msreserve.reservation/src/main/java/com/ikth/apps/msreserve/reservation/ReservationApplication.java
@@ -3,9 +3,11 @@ package com.ikth.apps.msreserve.reservation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+@ComponentScan(basePackages={"com.ikth.apps.msreserve"})
 public class ReservationApplication {
 
 	public static void main(String[] args) {
