80eb71b2650e133d582cdfe82203b2048126d402
ikthnism01 <ikthnism01@gmail.com>
2019-06-20 20:46:45 +0900

    zuul proxy server spring-config add.

--- msreserve.zuul.proxy/src/main/java/com/ikth/apps/msreserve/zuul/proxy/ZuulProxyApplication.java
+++ msreserve.zuul.proxy/src/main/java/com/ikth/apps/msreserve/zuul/proxy/ZuulProxyApplication.java
@@ -3,10 +3,8 @@ package com.ikth.apps.msreserve.zuul.proxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @EnableZuulProxy
-@EnableResourceServer
 @SpringBootApplication
 public class ZuulProxyApplication {
 
