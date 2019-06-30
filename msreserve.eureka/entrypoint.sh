10713e201cb4a270275e1e3979f54e87bc54c03d
ikthnism01 <ikthnism01@gmail.com>
2019-06-30 22:31:33 +0900

    docker volume 설정(toolbox 환경이기에 virtual box 설정 수정 필요) 및 docker compose 적용 완료. 전 프로젝트 적용 필요

--- msreserve.eureka/entrypoint.sh
+++ msreserve.eureka/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./wait-for-it.sh -t 0 config-server:8760 -- echo "config boot"
-java -Dspring.profiles.active=dev -jar /app/app.jar
+/app/wait-for-it.sh -t 0 config-server:8760 -- echo "config boot"
+java -Dspring.profiles.active=dev -jar /app/msreserve.eureka-0.0.1-SNAPSHOT.jar
