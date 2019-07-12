5cd704cc1d8fc13c4547caa3d2aa8ccbb58c4e17
taehoonkim <taehoon.kim@bankwareglobal.com>
2019-07-12 17:45:18 +0900

    zuul proxy 서버에 auth 처리 하려고 하는데 왜 자꾸 access denied가..? token 없으면 get token을 해야 하는거 아닌가..

--- msreserve.auth/src/main/java/com/ikth/apps/msreserve/auth/config/CustomOAuth2ServerConfiguration.java
+++ msreserve.auth/src/main/java/com/ikth/apps/msreserve/auth/config/CustomOAuth2ServerConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
@@ -44,6 +45,12 @@ public class CustomOAuth2ServerConfiguration extends AuthorizationServerConfigur
 		
 //		필요 없음
 //		clients.withClientDetails(new JdbcClientDetailsService(dataSource));
+	}
+	
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		super.configure(security);
+		security.checkTokenAccess("permitAll()");
 	}
 	
 	@Bean
