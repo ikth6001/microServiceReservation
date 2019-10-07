a15eb4f0310631b4185b0c26a139f2d6c3a796fa
ikthnism01 <ikthnism01@gmail.com>
2019-10-08 00:08:01 +0900

    eureka가 down된 서버 unregist하도록 하는 로직 오픈소스 분석해서 찾아보자

--- msreserve.web.ui/lib/ipMgr.js
+++ msreserve.web.ui/lib/ipMgr.js
@@ -1,5 +1,6 @@
 // 외부 사이트에 get 요청을 보내서 public ip를 알아내는 방식인데..
-const http= require('http');
+var http= require('http');
+var ip= require('ip');
 
 var options= {
 	host: 'api.ipify.org'
@@ -17,5 +18,6 @@ var options= {
 module.exports.getIp= function(fnc) {
 	// docker인 경우와 host인 경우의 구별이 필요한듯.. 추가로 클라우드에 올라갈 경우 public 이어야 하는데...
 	// 이 부분은 profile을 통해서 구별하도록 기능 개선이 필요할 듯. 일단은 하드코딩
-	fnc('192.168.123.104');
+	// fnc('192.168.123.104');
+	fnc(ip.address());
 }
